4aeabab86a936d1005999a7d98d843884d8162c7
Asad <asad@gmail.com>
2026-01-29 11:07:13 +0500

    ---

--- client/src/features/Form.jsx
+++ client/src/features/Form.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Form = () => {
+
+  const [nameField, setNameField] = useState("");
+
+  const handleSubmission = async(e) => {
+    try{
+      e.preventDefault();
+
+      const response = await axios.post("http://localhost:3000/api/create", {
+        nameField
+      });
+      console.log(response);
+    }
+    catch(err){
+      console.log("Error Submitting Form", err);
+    }
+  }
+
   return (
     <>
     <div className='flex justify-center py-24'>
-    <form>
+    <form onSubmit={handleSubmission}>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
         <legend className="fieldset-legend">ADD User</legend>
         <div className="join">
-          <input type="text" className="input join-item" placeholder="User name" />
+          <input type="text" className="input join-item" placeholder="User name" value={nameField} onChange={(e) => setNameField(e.target.value)} />
           <button className="btn join-item">save</button>
         </div>
       </fieldset>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/api/create", async(req, res) => {
+    try{
+        console.log(req);
+    }
+    catch(err){
+        console.log(err);
+    }
+})
+
 
 
 
